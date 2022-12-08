40028a2223a62a079a49175075eeb138c56ccc36
Artur <wasikartur87@gmail.com>
2022-12-08 11:11:53 +0100

    misja-7

---
+++ projects/space-app/src/app/modules/labs/pipes/moon-name/map-to-moon-name.ts
@@ -0,0 +1,15 @@
+import { Observable, pipe, UnaryFunction } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Moon } from 'space-api/types';
+
+export function mapToMoonName(moonId: number): UnaryFunction<Observable<Moon[]>, Observable<string>> {
+    return pipe(
+        map((moons) => {
+            const moon = moons.find(m => m.id === moonId);
+        if (!moon) {
+           throw new Error(`Could not find moon for id ${moonId}`);
+      }
+     return `${moon.name} (${moon.planet})`;
+   })
+ );
+}
